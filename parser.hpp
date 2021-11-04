4b42e338081f7d120d240381f83a5d4a6bbfa495
Peter-Massarello <pmassarello@gmail.com>
2021-11-03 19:33:52 -0500

    add grammar functions

--- parser.hpp
+++ parser.hpp
@@ -4,5 +4,25 @@
 #include "scanner.hpp"
 
 void parse(vector<Token*> tokens);
+void Program();
+void Block();
+void Vars();
+void Expression();
+void N();
+void A();
+void M();
+void R();
+void Stats();
+void Mstat();
+void Stat();
+void On();
+void Out();
+void If();
+void Loop();
+void Assign();
+void RO();
+void label();
+void Goto();
+void error();
 
 #endif
